9cf6b780f808517413870f99d12b1c8ed9a6e373
jero <aneurysmjs@outlook.com>
2019-09-20 05:33:40 +0300

    feat (urlParser): get HTTP method resolves: #1

--- src/core/urlParser.mjs
+++ src/core/urlParser.mjs
@@ -3,18 +3,20 @@ import url from 'url';
 /**
  * @param { Object } req - the request object
  * @param { String } req.url
+ * @param { String } req.method
  */
-export default function urlParser(req) {
-  console.log('req.url', req.url);
-  
+export default function urlParser(req) {  
   // get the url and parse it
   const parsedUrl = url.parse(req.url);
   // extract path
   const urlPath = parsedUrl.pathname;
   // trim slashes
   const trimmedPath = urlPath.replace(/^\/+|\/+$/g, '');
+  // get method
+  const method = req.method;
 
   return {
-    trimmedPath
+    method,
+    trimmedPath,
   };
 }
